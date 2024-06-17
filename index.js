7b569a965cb58fd3292381858219210491e7982b
Fran Vlahovic <hovla.office@gmail.com>
2024-06-17 22:46:02 +0200

    Update index.js Created two new variables to keep track of score. Created function that takes human and computer choices. Function increments the round winners score and logs a winner announcement.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let humanScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice (){
     const pcChoice = Math.floor(Math.random() * 3) + 1;
     
@@ -14,7 +18,7 @@ function getComputerChoice (){
     }
 }
 function getHumanChoice (){
-    let userChoice = prompt("Enter Rock, Paper or Scissors: ")
+    let userChoice = prompt("Enter Rock, Paper or Scissors: ").toLowerCase();
 
     if (userChoice === "rock" || "Rock") {
         return "rock";
@@ -26,3 +30,46 @@ function getHumanChoice (){
         return "scissors";
     }
 }
+function playRound(userChoice, pcChoice) {
+  if (userChoice === "rock" && pcChoice === "rock"){
+    console.log("It's a tie!");
+  }
+  else if (userChoice === "rock" && pcChoice === "paper") {
+    computerScore++;
+    console.log("You lose! Paper beats Rock.");
+  }
+  else if (userChoice === "rock" && pcChoice === "scissors") {
+    humanScore++;
+    console.log("You win! Rock beats Scissors.");
+  }
+  //
+  else if (userChoice === "paper" && pcChoice === "paper"){
+    console.log("It's a tie!");
+  }
+  else if (userChoice === "paper" && pcChoice === "scissors") {
+    computerScore++;
+    console.log("You lose! Scissors beats Paper.");
+  }
+  else if (userChoice === "paper" && pcChoice === "rock") {
+    humanScore++;
+    console.log("You win! Paper beats Rock.");
+  }
+  //
+  else if (userChoice === "scissors" && pcChoice === "scissors"){
+    console.log("It's a tie!");
+  }
+  else if (userChoice === "scissors" && pcChoice === "rock") {
+    computerScore++;
+    console.log("You lose! Rock beats Scissors.");
+  }
+  else if (userChoice === "scissors" && pcChoice === "paper") {
+    humanScore++;
+    console.log("You win! Scissors beats Paper.");
+  }
+}
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
