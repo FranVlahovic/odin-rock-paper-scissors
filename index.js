fa9a0a6acf0b08bff5ad2eba0fc47f8b8471b8ee
Fran Vlahovic <hovla.office@gmail.com>
2024-06-18 22:35:12 +0200

    Update Index.js Added new function. Changed inside the function for game to be played only for 5 rounds. Moved score tracking inside new function, now calls winner at the end. Score is now displayed whilst playing the game until end of the game. Game logic is finished, GUI awaits in the future.

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
-
 function getComputerChoice (){
     const pcChoice = Math.floor(Math.random() * 3) + 1;
     
@@ -30,46 +26,68 @@ function getHumanChoice (){
         return "scissors";
     }
 }
-function playRound(userChoice, pcChoice) {
-  if (userChoice === "rock" && pcChoice === "rock"){
-    console.log("It's a tie!");
-  }
-  else if (userChoice === "rock" && pcChoice === "paper") {
-    computerScore++;
-    console.log("You lose! Paper beats Rock.");
-  }
-  else if (userChoice === "rock" && pcChoice === "scissors") {
-    humanScore++;
-    console.log("You win! Rock beats Scissors.");
-  }
-  //
-  else if (userChoice === "paper" && pcChoice === "paper"){
-    console.log("It's a tie!");
-  }
-  else if (userChoice === "paper" && pcChoice === "scissors") {
-    computerScore++;
-    console.log("You lose! Scissors beats Paper.");
-  }
-  else if (userChoice === "paper" && pcChoice === "rock") {
-    humanScore++;
-    console.log("You win! Paper beats Rock.");
-  }
-  //
-  else if (userChoice === "scissors" && pcChoice === "scissors"){
-    console.log("It's a tie!");
-  }
-  else if (userChoice === "scissors" && pcChoice === "rock") {
-    computerScore++;
-    console.log("You lose! Rock beats Scissors.");
-  }
-  else if (userChoice === "scissors" && pcChoice === "paper") {
-    humanScore++;
-    console.log("You win! Scissors beats Paper.");
-  }
+function playGame(playRound){
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(userChoice, pcChoice) {
+        if (userChoice === "rock" && pcChoice === "rock"){
+            console.log("It's a tie!");
+        }
+        else if (userChoice === "rock" && pcChoice === "paper") {
+            computerScore++;
+            console.log("You lose! Paper beats Rock.");
+        }
+        else if (userChoice === "rock" && pcChoice === "scissors") {
+            humanScore++;
+            console.log("You win! Rock beats Scissors.");
+        }
+        //
+        else if (userChoice === "paper" && pcChoice === "paper"){
+            console.log("It's a tie!");
+        }
+        else if (userChoice === "paper" && pcChoice === "scissors") {
+            computerScore++;
+            console.log("You lose! Scissors beats Paper.");
+        }
+        else if (userChoice === "paper" && pcChoice === "rock") {
+            humanScore++;
+            console.log("You win! Paper beats Rock.");
+        }
+        //
+        else if (userChoice === "scissors" && pcChoice === "scissors"){
+            console.log("It's a tie!");
+        }
+        else if (userChoice === "scissors" && pcChoice === "rock") {
+            computerScore++;
+            console.log("You lose! Rock beats Scissors.");
+        }
+        else if (userChoice === "scissors" && pcChoice === "paper") {
+            humanScore++;
+            console.log("You win! Scissors beats Paper.");
+        }
+        else {
+            console.log("Invalid input. Try again!")
+        }
+        console.log(`Human Score: ${humanScore} | Computer Score: ${computerScore}`);
+    }
+
+    for (let i=0; i<5; i++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+    
+    if (humanScore > computerScore) {
+        console.log("Congratulations! You won!")
+    }
+    
+    else if (humanScore < computerScore) {
+        console.log("Game over. You lost!")
+    }
+
+    else if (humanScore === computerScore) {
+        console.log("It's a tie. Try again!")
+    }
 }
-
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
